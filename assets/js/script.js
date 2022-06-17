e8ac9b3c4433f76b4f9bd51015c6c883db7ccccc
Moonman9 <zzepold97@gmail.com>
2022-06-16 20:40:18 -0700

    block changes based on time added time at top

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+$('.saveBtn').on('click', function(){
+    var body = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+
+    localStorage.setItem(time, body);
+})
+
+
+function timeBlockClass() {
+    var currentTime = moment();
+
+    $(".time-block").each(function() {
+        var blockTime = parseInt($(this).attr("id"));
+
+        if(blockTime < currentTime) {
+            $(this).addClass('past');
+        } else if(blockTime === currentTime) {
+            $(this).addClass('present');
+        } else {
+            $(this).addClass('future');
+        }
+    })
+};
+
+
+
+timeBlockClass();
