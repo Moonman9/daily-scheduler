345a81bcb713a4b4f07f9478fc76ed85f4fecd69
Moonman9 <zzepold97@gmail.com>
2022-06-17 10:38:59 -0700

    final running application meeting criteria and containg comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
+//use moment to display time with defined format
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
+//looks for saveBtn class button click to save in local storage 
+//saves by key: id and its contents: body
 $('.saveBtn').on('click', function(){
     var body = $(this).siblings('.body').val();
     var time = $(this).parent().attr('id');
@@ -9,6 +12,7 @@ $('.saveBtn').on('click', function(){
     localStorage.setItem(time, body);
 });
 
+//displays timeblock with appropraite saved local storage
 $('#9 .body').val(localStorage.getItem('9'));
 $('#10 .body').val(localStorage.getItem('10'));
 $('#11 .body').val(localStorage.getItem('11'));
@@ -19,6 +23,8 @@ $('#15 .body').val(localStorage.getItem('15'));
 $('#16 .body').val(localStorage.getItem('16'));
 $('#17 .body').val(localStorage.getItem('17'));
 
+//gets current time and compares to the time block id to check 
+//if class applied should be past, present, or future.
 function timeBlockClass() {
     var currentTime = moment().hour();
 
@@ -37,6 +43,5 @@ function timeBlockClass() {
     })
 };
 
-
-
+// calls funtion to run
 timeBlockClass();
