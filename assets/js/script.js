b3c33fffd61d1dd8a542da62f44d4be341e72f42
Moonman9 <zzepold97@gmail.com>
2022-06-17 10:10:19 -0700

    changed box classs to appropriate colored based on past, present, or future.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 $('.saveBtn').on('click', function(){
-    var body = $(this).siblings('.description').val();
+    var body = $(this).siblings('.body').val();
     var time = $(this).parent().attr('id');
-
+    console.log(time)
+    console.log(body)
+    console.log(this)
     localStorage.setItem(time, body);
-})
+});
 
 
 function timeBlockClass() {
-    var currentTime = moment();
+    var currentTime = moment().hour();
 
     $(".time-block").each(function() {
         var blockTime = parseInt($(this).attr("id"));
 
+        console.log(blockTime);
+        console.log(currentTime);
         if(blockTime < currentTime) {
             $(this).addClass('past');
         } else if(blockTime === currentTime) {
